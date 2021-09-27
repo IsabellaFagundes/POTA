58122fd25d34d7426eca667fba28ede2c4d28b0a
Isabella Fagundes Barão <bellabarao@icloud.com>
2021-09-27 18:52:08 -0300

    comentários

--- aula05/AppPesquisa.java
+++ aula05/AppPesquisa.java
@@ -1,34 +1,34 @@
 public class AppPesquisa {
     public static void main(String[] args) {
         final int TAM_VETOR = 20;
-        int[] vetor = new int[TAM_VETOR];
+        int[] vetor = new int[TAM_VETOR]; //cria um vetor com tamanho 20
 
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = (int) (Math.random() * TAM_VETOR);
+        for (int i = 0; i < vetor.length; i++) { //não tem necessidade desse for, se tivermos o método gerarVetor
+            vetor[i] = (int) (Math.random() * TAM_VETOR); //gera os numeros do vetor de forma aleatória, tem numero de 0 a 20 (tamanho do vetor)
         }
 
-        VetorPesquisa pesquisa = new VetorPesquisa(vetor);
+        VetorPesquisa pesquisa = new VetorPesquisa(vetor); //faz a passagem da referencia do vetor acima para a classe VetorPesquisa
 
-        System.out.println(pesquisa.exibirVetor());
+        System.out.println(pesquisa.exibirVetor()); //aqui eu chamo o método para printar o vetor para o usuário
 
-        int chave = 8;
+        int chave = 8; //chave de busca
 
         System.out.println("Busca do valor: " + chave);
         
-        System.out.println(pesquisa.buscaSeq(chave));
-        System.out.println("Comparações: " + pesquisa.getComparacoes());
+        System.out.println(pesquisa.buscaSeq(chave)); //aqui chama o método que pesquisa o valor da chave no vetor
+        System.out.println("Comparações: " + pesquisa.getComparacoes()); //exibe a contagem de quantas vezes entrou no if (quantas comparações foram feitas)
 
-        System.out.println("Todos: " + pesquisa.buscaSeqTodas(chave));
-        System.out.println("Comparações: " + pesquisa.getComparacoes());
+        System.out.println("Todos: " + pesquisa.buscaSeqTodas(chave)); //chama o método de pesquisar todas as posições que encontrar a chave
+        System.out.println("Comparações: " + pesquisa.getComparacoes()); //exibe a contagem de quantas vezes entrou no if (quantas comparações foram feitas)
 
         System.out.println("Recursivo: " + pesquisa.buscaSeqRec(chave));
         
-        pesquisa.gerarVetorOrdenado();
-        System.out.println(pesquisa.exibirVetor());
-        System.out.println("Busca 8: " + pesquisa.buscaSeqOrdenado(chave));
-        System.out.println("Comparações: " + pesquisa.getComparacoes());
+        pesquisa.gerarVetorOrdenado(); //chama método que gera o vetor ordenado
+        System.out.println(pesquisa.exibirVetor()); //chama método que exibe vetor
+        System.out.println("Busca 8: " + pesquisa.buscaSeqOrdenado(chave)); //chama método que pesquisa a chave no vetor ordenado
+        System.out.println("Comparações: " + pesquisa.getComparacoes()); //exibe a contagem de quantas vezes entrou no if (quantas comparações foram feitas)
         System.out.println("Busca 8: " + pesquisa.buscaSeqOrdenado(chave+1));
-        System.out.println("Comparações: " + pesquisa.getComparacoes());
+        System.out.println("Comparações: " + pesquisa.getComparacoes()); //exibe a contagem de quantas vezes entrou no if (quantas comparações foram feitas)
     }
 
 }
--- aula05/VetorPesquisa.java
+++ aula05/VetorPesquisa.java
@@ -1,48 +1,61 @@
 
 public class VetorPesquisa {
     private int[] v;
-    private int comparacoes;
+    private int comparacoes; //atributo da classe, permite mostrar esse valor no app 
 
-    public VetorPesquisa(int[] v) {
+    /*private int v[]; -- slide
+
+    public int buscaSeq(int key) {
+        for (int i = 0; i < v.length; i++) {
+            if (key == v[i]){
+                return (i); // posição no vetor
+            }
+        }
+        return (-1); // Não encontrou
+    }*/
+
+    public VetorPesquisa(int[] v) { //validação para garantir que o vetor exista
         if (v != null) {
             this.v = v;
-        } else {
+        } else { //caso não existir, cria um vetor com um tamanho 10
             v = new int[10];
         }
     }
 
-    public VetorPesquisa(int tam) {
+    public VetorPesquisa(int tam) { //sobrecarga de métodos, cria um vetor a partir de um número recebido como parâmetro
         v = new int[tam];
     }
 
-    public int getComparacoes() {
+    public int getComparacoes() { //permite ler o valor (não permite alterar)
         return comparacoes;
     }
 
-    public void gerarVetor() {
+    public void gerarVetor() { //método para gerar o vetor aleatório
         for (int i = 0; i < v.length; i++) {
             v[i] = (int) (Math.random() * v.length);
         }
     }
 
-    public void gerarVetorOrdenado() {
+    public void gerarVetorOrdenado() { //método para gerar um vetor ordenado
         for (int i = 0; i < v.length; i++) {
-            v[i] = i * 2;
+            v[i] = i * 2; //gera numeros pares, multiplos de 2
         }
     }
 
-    public String exibirVetor() {
-        String aux = "";
+    public String exibirVetor() { //método para exibir o vetor
+        String aux = ""; //string auxiliar
         for (int numero : v) {
-            aux += numero + " ";
+            aux += numero + " "; //assim ele gera uma lista com o número, um espaço e outro número
         }
         return aux;
     }
 
-    public int buscaSeq(int key) {
-        comparacoes = 0;
+    //Algoritmo linear, pior caso Ó grande de N (tamanho do vetor), quanto maior o tamanho do vetor, maior o tempo (linearmente proporcional, aumenta um N, aumenta um no tempo)
+    //É um allgoritmo bom, mas não o melhor possível
+    public int buscaSeq(int key) { //método para buscar a chave dentro do vetor
+        comparacoes = 0; //contador iniciando em 0 ele conta quantas vezes vai entrar no if (numero de comparações)
         for (int i = 0; i < v.length; i++) {
-            comparacoes++;
+            comparacoes++; //incrementa o contador
             if (key == v[i]) {
                 return (i); // posição no vetor
             }
@@ -50,25 +63,31 @@ public class VetorPesquisa {
         return (-1); // Não encontrou
     }
 
-    // um método que retorna todas as posições da chave
+    // um método que retorna todas as posições que a chave aparece no vetor
+    //esse método roda menos vezes que o buscaSeq em média, pois nem sempre vai rodar até o final do vetor
+    //o pior caso continua o mesmo, Ó grande de N, e linear
     public String buscaSeqTodas(int key) {
-        comparacoes = 0;
-        String aux = "";
+        comparacoes = 0; //contador iniciando em 0 ele conta quantas vezes vai entrar no if (numero de comparações)
+        String aux = ""; //uma string auxiliar
         for (int i = 0; i < v.length; i++) {
-            comparacoes++;
-            if (key == v[i]) {
-                aux += i + " ";
+            comparacoes++; //incrementa o contador
+            if (key == v[i]) { //aonde achar a chave
+                aux += i + " "; //retorne a string com os indices
             }
         }
-        return (aux.length() > 0) ? aux : "-1";
+        return (aux.length() > 0) ? aux : "-1"; //se o tamanho do aux for > 0 retorna aux, caso contrário retorna -1 (vazio)
     }
 
     // um método que sabe que o vetor está ordenado ?
     // 2 4 5 7 8 9 10 {k = 6}
+
+    //esse método para antes de chegar no final
+    //esse método roda menos vezes que o buscaSeqTodas em média
+    //o pior caso continua o mesmo, Ó grande de N, e linear
     public int buscaSeqOrdenado(int key) {
-        comparacoes = 0;
-        for (int i = 0; i < v.length && (key >= v[i]); i++) {
-            comparacoes++;
+        comparacoes = 0; //contador iniciando em 0 ele conta quantas vezes vai entrar no if (numero de comparações)
+        for (int i = 0; i < v.length && (key >= v[i]); i++) { //ele verifica se o conteúdo é maior ou igual do que eu estou procurando, se for maior eu posso parar
+            comparacoes++; //incrementa o contador
             if (key == v[i]) {
                 return (i); // posição no vetor
             }
@@ -76,21 +95,26 @@ public class VetorPesquisa {
         return (-1); // Não encontrou
     }
 
-    public int buscaSeqRec(int key) {
+
+    //ver o método de baixo primeiro para entender
+    public int buscaSeqRec(int key) { //sobrecarga do método, para garantir que o usuário não precise inserir o tamanho
         return buscaSeqRec(key, v.length);
     }
-    
+
     // Escreva a versão recursiva do método BuscaSequencial
-    private int buscaSeqRec(int key, int tam) {
-        if (tam == 0) { // caso base (não achou)
+    //esse método analisa o vetor do ultimo elemento para o primeiro, do final pro começo
+    private int buscaSeqRec(int key, int tam) { //privado para não mudar o tamanho do método
+        // caso base (não achou) 
+        if (tam == 0) { //0 porque eu percorri do ultimo indice até o indice 0 e não achei
             return -1;
         }
-        if (v[tam - 1] == key) { // caso base (achou)
-            return tam - 1;
+
+        // caso base (achou)
+        if (v[tam - 1] == key) {  //se o tamanho do vetor é 10, o ultimo elemento é o 0, ou seja tam -1 (ultimo elemento do vetor)
+            return tam - 1; //então se esse ultimo elemento é o que eu estou procurando, retorna justamente o indice tam -1
         }
 
         // geral
-        return buscaSeqRec(key, tam - 1);
+        return buscaSeqRec(key, tam - 1); //retorno a chamada recursiva passando a chave e o tam - 1)
     }
 }
-
