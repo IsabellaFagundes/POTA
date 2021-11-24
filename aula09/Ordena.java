b317f4dd6b9122656c6ed9feb03ec99d1d957c6f
Isabella Fagundes Barão <bellabarao@icloud.com>
2021-11-23 21:52:19 -0300

    aula09

--- aula09/Ordena.java
+++ aula09/Ordena.java
@@ -1,31 +1,67 @@
 public class Ordena {
-    private static void troca(int[] v, int indice1, int indice2){
+    
+    public static enum Tipo {
+        CRESCENTE, DECRESCENTE
+    }
+
+    private static void troca(int[] v, int indice1, int indice2) {
         int temp = v[indice1];
         v[indice1] = v[indice2];
         v[indice2] = temp;
     }
 
-    private static void quickSort(int[] v, int lo, int hi) {
+    private static void quickSorCrescente(int[] v, int lo, int hi) {
         int esq = lo;
         int dir = hi;
+
         int pivo = v[ (esq + dir) / 2];
 
-        while (esq <= dir) {
-            while (v[esq] < pivo){
+        while ( esq <= dir ) { // enquanto os indices não se cruzarem
+            while (v[esq] < pivo) {
                 esq++;
             }
-            while (v[dir] > pivo){
-                dir --; 
+            while (v[dir] > pivo) {
+                dir--;
             }
-            
-            if (esq <= dir) {
-                troca (v, esq, dir);
-                esq ++;
-                dir --;
-                
+            if( esq <= dir ) {
+                troca(v, esq, dir);
+                esq++;
+                dir--;
             }
         }
-        if (dir - lo > 0) quickSort(v, lo, dir);
-        if (hi - esq > 0) quickSort(v, lo, esq);
+        if(dir - lo > 0) quickSorCrescente(v, lo, dir); // chamada recursiva do vetor à esquerda
+        if(hi - esq > 0) quickSorCrescente(v, esq, hi); // chamada recursiva do vetor à direita
     }
+
+    private static void quickSorDecrescente(int[] v, int lo, int hi) {
+        int esq = lo;
+        int dir = hi;
+
+        int pivo = v[ (esq + dir) / 2];
+
+        while ( esq <= dir ) { // enquanto os indices não se cruzarem
+            while (v[esq] > pivo) {
+                esq++;
+            }
+            while (v[dir] < pivo) {
+                dir--;
+            }
+            if( esq <= dir ) {
+                troca(v, esq, dir);
+                esq++;
+                dir--;
+            }
+        }
+        if(dir - lo > 0) quickSorDecrescente(v, lo, dir); // chamada recursiva do vetor à esquerda
+        if(hi - esq > 0) quickSorDecrescente(v, esq, hi); // chamada recursiva do vetor à direita
+    }
+
+    public static void quickSort(int[] v, Tipo tipo) {
+        if(tipo == Tipo.CRESCENTE)
+            quickSorCrescente(v, 0, v.length-1);
+        else
+            quickSorDecrescente(v, 0, v.length-1);
+    }
+
 }
+
